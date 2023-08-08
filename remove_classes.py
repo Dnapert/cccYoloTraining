41053fdc596a85189451158266761d332b335fc4
Dnapert <dannapert@gmail.com>
2023-08-08 16:14:18 -0400

    first commit

---
+++ remove_classes.py
@@ -0,0 +1,46 @@
+import json
+def remove_classes_from_annotations(annotations_path, classes_to_remove, 
+output_path):
+    """
+    Remove classes from annotations file
+    :param annotations_path: Path to annotations file
+    :param classes_to_remove: List of classes to remove
+    :param output_path: Path to output file
+    :return: None
+
+    0:plastic_bag
+    1:plastic_bottle
+    2:plastic_cap
+    3:plastic_container
+    4:plastic_wrapper
+    5:plastic_other
+    6:foam_container
+    7;Foam_other
+    8:glass_bottle
+    9:paper_container
+    10:paper_other
+    11:Metal_cap
+    12:Metal_can
+    13:ppe
+    14:misc
+
+
+    """
+    with open(annotations_path, 'r') as f:
+        annotations = json.load(f)
+
+    annotations['annotations'] = [c for c in annotations['annotations'] if 
+c['category_id'] not in classes_to_remove]
+    annotations['categories'] = [c for c in annotations['categories'] if 
+c['id'] not in classes_to_remove]
+
+
+    with open(output_path, 'w') as f:
+        json.dump(annotations, f)
+        
+classes_to_remove = [9,10,11,12,13,14]   
+annotations_path = "annotation_coco_format.json"
+output_path = "annotation_coco_format_3.json"
+
+remove_classes_from_annotations(annotations_path, classes_to_remove, 
+output_path)

78b64a8ce051e2532911986679b6cda72f705e90
Dnapert <dannapert@gmail.com>
2023-08-10 07:29:36 -0400

    requirements.txt

--- resize.py
+++ resize.py
@@ -1,13 +1,13 @@
 import cv2
 
 import os
-from utils import *
+from pathlib import Path
 
 
 def resize_images(image_dir, width, height,output_dir):
         fn = Path(output_dir) 
         fn.mkdir(parents=True, exist_ok=True)
-        
+
         for filename in os.listdir(image_dir):
             
             image = cv2.imread(os.path.join(image_dir,filename))

cb67e5ce4338f96b5af8d3fb00a3dba29749a69f
tnghieu <tnghieu@gmail.com>
2023-08-29 13:19:01 -0700

    Update to parameters

--- upload_data.py
+++ upload_data.py
@@ -1,16 +1,17 @@
 import requests
 import argparse
 import os
+import json
 
 url = "https://us-central1-cleancurrentscoalition.cloudfunctions.net/upload-images"
 
 
-def upload_data(device_id, raw_image_path, annotated_image_path, data):
+def upload_data(device_id, image_file_path, bounding_box_file_path, data):
     files = {
-        "device_id": (None, device_id),
+        "device_id": (None, str(device_id)),
         "contents_data": (None, data),
-        "raw_image_file": ("", open(raw_image_path, "rb")),
-        "annotated_image_file": ("", open(annotated_image_path, "rb")),
+        "image_file": open(image_file_path, "rb"),
+        "bounding_box_file": open(bounding_box_file_path, "r"),
     }
     
     response = requests.post(url, files=files)
@@ -21,17 +22,16 @@ def upload_data(device_id, raw_image_path, annotated_image_path, data):
         print(f"Error uploading images. Status code: {response.status_code}")
         print(response.text)
 
-
-
-parser = argparse.ArgumentParser(description='Upload raw and annotated images and trash collected data to the server')
+parser = argparse.ArgumentParser(description='Upload trash wheel image, bounding box data, and trash collected data to the server')
 parser.add_argument('--device_id', type=int, help='unique id of device')
-parser.add_argument('--raw_image_path', type=argparse.FileType('rb'), help='directory path to raw image of trash on the trashwheel')
-parser.add_argument('--annotated_image_path', type=argparse.FileType('rb'), help='directory path to annotated image of trash on the trashwheel')
-parser.add_argument('--data', type=str, default={}, help='collected trash data')
+parser.add_argument('--image_file_path', type=str, help='directory path to image of trash on the trashwheel')
+parser.add_argument('--bounding_box_file_path', type=str, help='directory path to bounding box data of the image of trash on the trashwheel')
+parser.add_argument('--data', type=str, default='{}', help='collected trash data')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    if not os.path.exists(args.raw_image_path) or not os.path.exists(args.annotated_image_path):
-        print("One or both image files do not exist or paths are invalid.")
+    if not os.path.exists(args.image_file_path) or not os.path.exists(args.bounding_box_file_path):
+        print("Image and bounding box data need to both be present")
     else:
-        upload_data(device_id=args.device_id, raw_image_path=args.raw_image_path, annotated_image_path=args.annotated_image_path, data=args.data)
+        json_data = json.dumps(json.loads(args.data))
+        upload_data(device_id=args.device_id, image_file_path=args.image_file_path, bounding_box_file_path=args.bounding_box_file_path, data=json_data)

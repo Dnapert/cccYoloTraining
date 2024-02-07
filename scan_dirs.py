8ea3900aa81faa10b1a29df1825a70bde2d58a06
Dan Napert <dannapert@gmail.com>
2024-02-07 10:15:04 -0500

    print num images

--- scan_dirs.py
+++ scan_dirs.py
@@ -7,12 +7,13 @@ def scan_dirs():
     Scan buckets for new images and call the auto_annotate function on them
     '''
     directories = ['/home/bucket-mounts/1','/home/bucket-mounts/2','/home/bucket-mounts/3']
-
+    total_images = 0
     for directory in directories:
        subdirs =  os.listdir(directory)
        for dir in subdirs:
            images = os.listdir(f'{directory}/{dir}/images')
-           print(len(images))
+           total_images += len(images)
+    print(f"Found {total_images} images")
            
       
 

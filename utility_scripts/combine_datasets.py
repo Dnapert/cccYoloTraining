ffe0d638248be87642b25a7b33429bca3abf9f0f
Dan Napert <dannapert@gmail.com>
2024-02-06 20:00:16 -0500

    bug fix for real this time

--- utility_scripts/combine_datasets.py
+++ utility_scripts/combine_datasets.py
@@ -36,20 +36,27 @@ def combine_datasets(annotation_file1, annotation_file2):
     
     # Remap the image ids to be unique across the two datasets
     maxn = len(data1['images'])
-    
+    print(maxn)
     data1_id_dict = {data1['images'][i]['id']: i +1 for i in range(maxn)}
+    
+    for i in range(len(data1['images'])):
+        data1['images'][i]['id'] = data1_id_dict[data1['images'][i]['id']]
     
     for i in range(len(data1['annotations'])):
         data1['annotations'][i]['id'] = i
         data1['annotations'][i]['image_id'] = data1_id_dict[data1['annotations'][i]['image_id']]
         
         
-    data2_id_dict = {data2['images'][i]['id']: i + maxn for i in range(len(data2['images']))}
-    
-    for i in range(len(data2['annotations'])):
-        data2['annotations'][i]['id'] = i + maxn
-        data2['annotations'][i]['image_id'] = data2_id_dict[data2['annotations'][i]['image_id']] 
         
+    data2_id_dict = {data2['images'][i]['id']: 1 + i + maxn for i in range(len(data2['images']))}
+    
+    for i in range(len(data2['images'])):
+        data2['images'][i]['id'] = data2_id_dict[data2['images'][i]['id']]
+        
+    for i in range(len(data2['annotations'])):
+        data2['annotations'][i]['id'] = 1 + i + maxn
+        data2['annotations'][i]['image_id'] = data2_id_dict[data2['annotations'][i]['image_id']] 
+             
 
     combined_annotations = data1['annotations'] + data2['annotations']
     combined_images = data1['images'] + data2['images']

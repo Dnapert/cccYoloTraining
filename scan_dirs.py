9a446603f8fd4fc719b89ff35d4b5653f33d8882
Dan Napert <dannapert@gmail.com>
2024-02-08 14:34:26 -0500

    scan dirs

--- scan_dirs.py
+++ scan_dirs.py
@@ -5,11 +5,8 @@ from auto_annotate import auto_annotate
 def scan_dirs():
     '''
     Scan buckets for new images and call the auto_annotate function on them
-   
-    
     '''
     directories = ['/home/bucket-mounts/1','/home/bucket-mounts/2','/home/bucket-mounts/3']
-    total_images = 0
     for directory in directories:
        subdirs =  os.listdir(directory)
        for dir in subdirs:

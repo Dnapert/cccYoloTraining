86e01031923606e582fab2c9bf806b99be1a2cdb
Dan Napert <dannapert@gmail.com>
2024-02-09 15:05:26 -0500

    lower

--- lowercase.py
+++ lowercase.py
@@ -7,9 +7,11 @@ def tolower(annotation_file):
     for i in range(len(annotation_file['annotations'])):
         # change all .JPG images to .jpg
         annotation_file['annotations'][i]['file_name'] = annotation_file['annotations'][i]['file_name'].split('.')[0] + '.jpg'
+    with open(annotation_file, 'w') as f:
+        json.dump(annotation_file, f, indent=4)
         
 argsparser = argparse.ArgumentParser()
-argsparser.add_argument('-a','--annotation_file',help='path to annotation file')
+argsparser.add_argument('-a',help='path to annotation file')
 args = argsparser.parse_args()
 
 if __name__ == '__main__':

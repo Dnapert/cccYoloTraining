8b7a7b2d79431d147a507e5ee75a316d97db773e
Dan Napert <dannapert@gmail.com>
2024-02-09 15:07:00 -0500

    typo

--- lowercase.py
+++ lowercase.py
@@ -3,12 +3,12 @@ import argparse
 
 def tolower(annotation_file):
     with open(annotation_file, 'r') as f:
-        annotation_file = json.load(f)
-    for i in range(len(annotation_file['annotations'])):
+        file = json.load(f)
+    for i in range(len(file['annotations'])):
         # change all .JPG images to .jpg
-        annotation_file['annotations'][i]['file_name'] = annotation_file['annotations'][i]['file_name'].split('.')[0] + '.jpg'
+        annotation_file['annotations'][i]['file_name'] = file['annotations'][i]['file_name'].split('.')[0] + '.jpg'
     with open(annotation_file, 'w') as f:
-        json.dump(annotation_file, f, indent=4)
+        json.dump(file, f, indent=4)
         
 argsparser = argparse.ArgumentParser()
 argsparser.add_argument('-a',help='path to annotation file')

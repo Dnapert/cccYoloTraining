fea347c27aff6cea1959eb647c37ff0a5da74a18
Dan Napert <dannapert@gmail.com>
2024-02-14 14:09:36 -0500

    non normalized

--- auto_single_dir.py
+++ auto_single_dir.py
@@ -51,17 +51,13 @@ def auto_annotate(model, image_dir,batch_size=12,move=False,output_dir="/home/tr
             image_id = len(data['images'])
             res = item.boxes.cpu().numpy()
             classes = res.cls
-            boxes = res.xywhn
+            boxes = res.xywh
             height,width = item.orig_shape
             file_name = batch[i].split('/')[-1]
             data['images'].append({"file_name":file_name,"id":image_id,"width":width,"height":height})
        
             for box,cls in zip(boxes,classes):
                 x,y,w,h = [float    (b) for b in box]
-                x = width * x
-                y = height * y
-                w = width * w
-                h = height * h
                 data['annotations'].append({
                     "id" : len(data['annotations']),
                     "image_id":image_id,

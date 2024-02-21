e27a07fc0723cde9146f524002d4970af271bd0e
tnghieu <tnghieu@users.noreply.github.com>
2024-02-21 12:46:30 -0800

    Update auto_single_dir.py

--- auto_single_dir.py
+++ auto_single_dir.py
@@ -51,7 +51,7 @@ def auto_annotate(model, image_dir, batch_size=12, move=False, output_dir="/home
     data = {'categories': custom_categories, 'images': [], 'annotations': []}
     print(model.names)
     name_to_id = {category['name']: category['id'] for category in custom_categories}
-    model_id_to_custom_category_id = {i : name_to_id.get(name, -1) for i, name in enumerate(model.names)}
+    model_id_to_custom_category_id = {i: name_to_id.get(name, -1) for i, name in model.names.items()}
     print(f'name_to_id: {name_to_id}')
     print(f'model_id_to_custom_category_id: {model_id_to_custom_category_id}')
     
